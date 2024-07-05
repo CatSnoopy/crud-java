ab190541ad93d4b94a374570c99753b35758c617
Viviana Luna <90412539+CatSnoopy@users.noreply.github.com>
2024-07-05 15:59:26 -0500

    Add files via upload

---
+++ crudoriginal/src/main/java/com/practica/crudoriginal/Business/UserBusiness.java
@@ -0,0 +1,65 @@
+package com.practica.crudoriginal.Business;
+
+import com.practica.crudoriginal.Dto.UserDTO;
+import com.practica.crudoriginal.Entity.UserEntity;
+import com.practica.crudoriginal.Service.UserService;
+import com.practica.crudoriginal.utilities.CustomException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserBusiness {
+
+    @Autowired
+    private UserService userService;
+
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    public List<UserDTO> findAll(){
+        try {
+            List<UserEntity> userEntities = userService.findAll();
+            List<UserDTO> userDTOList = new ArrayList<>();
+
+            for (UserEntity users : userEntities){
+                userDTOList.add(modelMapper.map(users, UserDTO.class));
+            }
+            return userDTOList;
+        }catch (Exception e){
+            throw new CustomException("Error getting user", e);
+        }
+
+    }
+    public Boolean create(UserDTO userDTO){
+        try {
+            UserEntity users = modelMapper.map(userDTO, UserEntity.class);
+            userService.save(users);
+            return true;
+        }catch (Exception e){
+            throw new CustomException("Error adding user", e);
+        }
+
+    }
+    public Boolean update(UserDTO userDTO){
+        try {
+            UserEntity users = modelMapper.map(userDTO, UserEntity.class);
+            userService.update(users);
+            return true;
+        }catch (Exception e){
+            throw new  CustomException("Error update user", e);
+        }
+
+    }
+    public Boolean delete(long id_documents){
+        try {
+            userService.delete(id_documents);
+            return true;
+        }catch (Exception e){
+            throw new CustomException("Error deleting user", e);
+        }
+    }
+
+}
---
+++ crudoriginal/src/main/java/com/practica/crudoriginal/Controller/UserController.java
@@ -0,0 +1,40 @@
+package com.practica.crudoriginal.Controller;
+
+import com.practica.crudoriginal.Business.UserBusiness;
+import com.practica.crudoriginal.Dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import com.practica.crudoriginal.utilities.ResponseHttpApi;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/crudoriginal/users")
+@CrossOrigin("*")
+public class UserController {
+
+    @Autowired
+    private UserBusiness userBusiness;
+
+    @GetMapping("/allusers")
+    public List<UserDTO> getAllUsers(){
+        return userBusiness.findAll();}
+
+    @PostMapping("/createusers")
+    public Map<String, Object> create(@RequestBody UserDTO userDTO) {
+        if (this.userBusiness.create(userDTO)) {
+            return ResponseHttpApi.responseHttpPost("holi", HttpStatus.CREATED);
+        } else {
+            return ResponseHttpApi.responseHttpPost("llorelo", HttpStatus.BAD_REQUEST);
+        }
+    }
+    @PutMapping("/updateusers")
+    public void update(@RequestBody UserDTO userDTO){
+        userBusiness.update(userDTO);}
+
+    @DeleteMapping("/deleteusers/{id}")
+    public void delete(@PathVariable("id") Long id){
+        userBusiness.delete(id);}
+}
