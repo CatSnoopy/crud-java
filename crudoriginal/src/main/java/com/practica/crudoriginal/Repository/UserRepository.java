4da7752df751c96feed741e8f8408342a09a2254
Viviana Luna <90412539+CatSnoopy@users.noreply.github.com>
2024-07-08 17:47:38 -0500

    Delete crudoriginal directory

--- crudoriginal/src/main/java/com/practica/crudoriginal/Repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.practica.crudoriginal.Repository;
-import com.practica.crudoriginal.Entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
